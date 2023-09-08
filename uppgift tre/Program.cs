5d28f5ac98caf1bf41b63a15f0161a38041b96e0
Admin <Admin@TEB123019>
2023-09-08 14:50:41 +0200

    Add project files.

---
+++ uppgift tre/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace uppgift_1._1
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("   /   \\");
+            Console.WriteLine("  /     \\");
+            Console.WriteLine(" /       \\");
+            Console.WriteLine("/________ \\");
+            Console.ReadKey();
+        }
+    }
+}
